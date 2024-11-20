034b23e9d69eb535da47401c8ff88f663f243671
margaux mazaleyras <margaux.crvx@gmail.com>
2024-11-20 14:19:47 +0100

    Initial Commit

---
+++ Peak_at_Bird/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Peak_at_Bird
+//
+//  Created by Margaux Mazaleyras on 20/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Peak_at_Bird/Peak_at_BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peak_at_BirdApp.swift
+//  Peak_at_Bird
+//
+//  Created by Margaux Mazaleyras on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Peak_at_BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
